de1f239430711cbf5a8c94f616bffd45cd64d7e0
Volha Taliaronak <olyatarelka@gmail.com>
2023-01-08 18:00:55 +0100

    Changes are need to set robot in the correct initial position

--- robot-playground-main/nao_commands/nao_stand_zero.py
+++ robot-playground-main/nao_commands/nao_stand_zero.py
@@ -1,4 +1,5 @@
 import sys
+import time
 sys.path.append("/home/volha/pynaoqi-python2.7-2.5.7.1-linux64/lib/python2.7/site-packages")
 from naoqi import ALProxy
 
@@ -12,7 +13,13 @@ def main():
 
     # Go to rest position
     #motionProxy.StandZero(
-    posture_service.goToPosture("StandZero", 0.5)	
+    # posture_service.goToPosture("StandZero", 0.5)	
+    motion =  ALProxy("ALMotion", robotIP, 9559) 
+    motion.setAngles("HeadPitch", 0.0, 0.15) 
+    motion.setAngles("HeadYaw", 0.0, 0.1)
+    # time.sleep(1)
+    # motion.setAngles("HeadYaw", 0.0, 0.0)
+    
 
     print "::::::::::::::::::::::::::::::::::::::"
     print "          NAO movement done          "
